ee3bf3f79035685858ad527b3d4b3cfa28e36814
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-02-23 20:24:54 +0100

    add img and h2

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,22 @@
 'use strict';
 
-const listCocktails = document.querySelector('.js-list-cocktails');
-const 
+const listMargarita = document.querySelector('.js-list-margarita');
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+//variable que guarda la información, datos de las margaritas
+let listMargaritaData = [];
 
-listCocktails.innerHTML = `
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    listMargaritaData = data.drinks;
+
+})
+
+//pintar un elemento de la lista
+function renderMargarita(drink) {
+    listMargarita.innerHTML = `<li>
+<img${drinks.strDrinkThumb}>
+<h2>${drinks.strDrink}</h2>
+</li>`;
+}
