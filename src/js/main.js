c5b5810b678444318e90d02dd45d3fc52241210b
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-02-24 13:41:53 +0100

    add drinks

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,39 @@
 'use strict';
 
-const listMargarita = document.querySelector('.js-list-margarita');
+const listDrinks = document.querySelector('.js-list-drinks');
 const inputUser = document.querySelector('.inputuser');
 const inputSearch = document.querySelector('.js-search');
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
 //variable que guarda la información
-let listMargaritaData = [];
+let listDrinksData = [];
 //variable del resto de cocktails
 // let listCocktail = [];
-//variable para meter los cócteles 
+//guardar la lista de favoritos 
+let listFavoritesData = [];
+let listFavorites = document.querySelector('.js-list-favorites');
 
 fetch(url)
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    listMargaritaData = data.drinks;
-    renderMargarita(listMargaritaData);
+    listDrinksData = data.drinks;
+    renderDrinks(listDrinksData);
 
 })
 
 //pintar un elemento de la lista
 //Algunas de los cócteles que devuelve el API no tienen imagen. En ese caso hay que mostrar una imagen de relleno. Podemos crear una imagen de relleno con el servicio de placeholder.com donde en la propia URL indicamos el tamaño, colores, texto:
-function renderMargarita(drinks) {
+function renderDrinks(drinks) {
   
   for (const drink of drinks) {
     if (drink.strDrinkThumb) {
-    listMargarita.innerHTML += `<li class="js-pack" id="${drink.idDrink}">
+    listDrinks.innerHTML += `<li class="js-pack" id="${drink.idDrink}">
      <h2 class="title">${drink.strDrink}</h2>
      <img src=${drink.strDrinkThumb}>
 </li>`;
 }
  else  {
-    listMargarita.innerHTML += `<li id="${drink.idDrink}">
+    listDrinks.innerHTML += `<li id="${drink.idDrink}">
      <h2 class="title">${drink.strDrink}</h2>
      <img src=https://via.placeholder.com/210x295/ffffff/666666/?text=TV>
      </li>`;
@@ -50,19 +52,21 @@ function hadleClickSearch(ev) {
   .then(data => {
     console.log(data);
     //añade la lista con los datos de los cocktails
-    listMargaritaData = data.drinks;
+    listDrinksData = data.drinks;
     //vaciate las margaritas predeterminadas
-    listMargarita.innerHTML='';
-    renderMargarita(listMargaritaData);
+    listDrinks.innerHTML='';
+    renderDrinks(listDrinksData);
 
 })
 }
+
+
 //llamarla fuera para hacerle click al pack 
 function handleClickPack(ev) {
   console.log('funciona')
   const id = ev.currentTarget.id
   ev.currentTarget.classList.toggle('selected')
-
+  //si el id es igual al elemento seleccionado, cogelo (find)
 
 
 }
@@ -71,6 +75,7 @@ function addEventDrink() {
   const pack = document.querySelectorAll('.js-pack');
   for (const li of pack)
   li.addEventListener('click', handleClickPack)
+
   
 }
 
