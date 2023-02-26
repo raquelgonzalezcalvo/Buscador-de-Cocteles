8d2227cdae98b53b1c86e718b5ce3bddf38d12c3
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-02-26 20:43:03 +0100

    add style

--- src/js/main.js
+++ src/js/main.js
@@ -3,22 +3,20 @@
 const listDrinks = document.querySelector('.js-list-drinks');
 const inputUser = document.querySelector('.js-inputuser');
 const inputSearch = document.querySelector('.js-search');
-const url =
-  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
-//variable global de las de las bebidas, guarda los datos
+const inputReset = document.querySelector('.js-reset');
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
 let listDrinksData = [];
-//guardar la lista de favoritos
 let listFavoritesData = [];
 const listFavorites = document.querySelector('.js-list-favorites');
 
-//un fetch para obtener los datos, cuándo se carga la página aparecen las margaritas
+
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
     listDrinksData = data.drinks;
     renderDrinks(listDrinksData);
   });
+
 //con el getItem obtenemos los datos de favoritos para mostrarlos en el navegador cuando la usuaria entra o refresca la página.
 function getItemFavorites() {
   //cuando queremos obtener los datos queremos utilizar los mismos tipos de datos(string,número,array, objetos....)
@@ -27,7 +25,7 @@ function getItemFavorites() {
     listFavoritesData = ilFavorites;
     //pintame mi lista de favoritos que ya está convertida con el parse
     renderFavorites(listFavoritesData);
-    console.log('hola');
+    
   }
 }
 //la estoy llamando para que obtenga los datos del localStorage
@@ -43,7 +41,7 @@ function renderDrinks(drinks) {
      <img src=${drink.strDrinkThumb} class="image">
 </li>`;
     } else {
-      listDrinks.innerHTML += `<li id="${drink.idDrink}">
+      listDrinks.innerHTML += `<li id="${drink.idDrink}>
      <h2 class="title">${drink.strDrink}</h2>
      <img src=https://via.placeholder.com/210x295/ffffff/666666/?text=TV class="image">
      </li>`;
@@ -56,13 +54,12 @@ function renderDrinks(drinks) {
 //boton de búsqueda
 function hadleClickSearch(ev) {
   ev.preventDefault();
-  const inputUserValue = inputUser.value.toLowerCase(); //para que nos pille mayúsculas y miniscúlas
+  const inputUserValue = inputUser.value.toLowerCase();
   fetch(
     `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputUserValue}`
   ) //aqui se añade a la url el input que ponga el usuario
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       //añade la lista con los datos de los cocktails
       listDrinksData = data.drinks;
       //vacia mi listado y me añade el que he buscado
@@ -74,14 +71,12 @@ function hadleClickSearch(ev) {
 //llamarla fuera para hacerle click al pack
 function handleClickPack(ev) {
   ev.currentTarget.classList.toggle('selected');
-  console.log('funciona');
   const idSelected = ev.currentTarget.id;
   const drinkSelected = listDrinksData.find(item => item.idDrink === idSelected);
   //busca la posición
   const drinkIndex = listFavoritesData.findIndex(item => item.idDrink === idSelected);
-  console.log(drinkIndex); //le doy a click -1
+  console.log(drinkIndex);
   if (drinkIndex === -1) {
-    //cuando lo seleccione lo añado a mi lista de favoritos
     //selecciono y lo añado abajo del todo de favorites
     listFavoritesData.push(drinkSelected);
   } else {
@@ -102,9 +97,17 @@ function renderFavorites(drinksFav) {
   }
 }
 
+//incluye la imagen y el titulo
 function addEventDrink() {
   const pack = document.querySelectorAll('.js-pack');
   for (const li of pack) li.addEventListener('click', handleClickPack);
 }
 
+//boton de reseteo
+function hadleClickReset(ev) {
+  ev.preventDefault();
+  listFavorites.innerHTML= '';
+}
+
 inputSearch.addEventListener('click', hadleClickSearch);
+inputReset.addEventListener('click', hadleClickReset);
